ff8cbfec2625887314b8e485fc1605e0af9e93b6
Kyle Dent <kdent@seaglass.com>
2023-08-03 17:31:49 -0400

    Adds docstring to unredact.py script file.

--- unredact.py
+++ unredact.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+"""Top level driver script for unredacting PDF files."""
+
 import sys
+
 import unredact
 
 if len(sys.argv) != 2:
@@ -11,4 +14,3 @@ pdf_file = sys.argv[1]
 output_pdf = unredact.get_output_filename(pdf_file)
 
 unredact.main(pdf_file, output_pdf)
-
