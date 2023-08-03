ee184776fa1afa56c5ab42e97f9664c5d8d5f5a8
Kyle Dent <kdent@seaglass.com>
2023-08-03 18:38:31 -0400

    Moves imports into unredact.py.

--- unredact.py
+++ unredact.py
@@ -2,13 +2,13 @@
 
 import sys
 
-import unredact
+from unredact.__main__ import get_output_filename, main
 
 if len(sys.argv) != 2:
     sys.stderr.write(f"usage: {sys.argv[0]} <pdf_file>\n")
     sys.exit(1)
 
 pdf_file = sys.argv[1]
-output_pdf = unredact.get_output_filename(pdf_file)
+output_pdf = get_output_filename(pdf_file)
 
-unredact.main(pdf_file, output_pdf)
+main(pdf_file, output_pdf)
