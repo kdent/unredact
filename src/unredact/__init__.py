db4a3ea6e38c6d2e693bfc2e294d20896e7134e3
Kyle Dent <kdent@ovidly.com>
2026-07-03 22:17:30 -0400

    Adds init file.

---
+++ src/unredact/__init__.py
@@ -0,0 +1,7 @@
+
+from unredact._version import version as __version__
+from unredact._version import version_tuple as __version_tuple__
+
+from unredact.core import UnredactPdf
+
+__all__ = ["UnredactPdf"]
